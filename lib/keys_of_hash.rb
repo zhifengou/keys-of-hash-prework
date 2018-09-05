12595eaadd7bfa86b2f25b1c53921c54a0f230ea
zhifeng ou <zhifengou@hotmail.com>
2018-09-05 22:02:52 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,15 +3,3 @@ class Hash
     # code goes here
   end
 end
-class Hash
-  def keys_of(arguments)	  def keys_of(*arguments)
-    # code goes here	    # code goes here
-		animals = []
-		self.each { |animal, place|
-			if arguments.include?(place)
-				animals << animal
-			end
-		}
-		return animals
-  end	  end
-end 	end
